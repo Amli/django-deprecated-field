97566813e47e206595e9ddf1b0c5880f47e92433
Emmanuel Magnier <emmanuel.magnier@opendatasoft.com>
2025-12-06 13:31:09 +0100

    Add python 3.9 compatibility

--- src/django_deprecated_field/deprecated_field.py
+++ src/django_deprecated_field/deprecated_field.py
@@ -1,9 +1,14 @@
-from typing import Any, TypeAlias
+from __future__ import annotations
+
+from typing import Any
 
 from django.conf import settings
 from django.db import models
 from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.models.expressions import Col, Expression
+
+# import from type_extensions required to support python 3.9
+from typing_extensions import TypeAlias
 
 if getattr(settings, "DEPRECATED_FIELD_USE_STRUCTLOG", False):
     from structlog import get_logger
